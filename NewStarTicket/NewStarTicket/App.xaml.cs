4477fcca9c00b519a70d4d3508e8685e2fbc687e
Metared04 <e.metared@gmail.com>
2025-08-28 16:31:53 +0200

    Pour le moment lorsqu'un utilisateur est dans la bdd cela redirige l'utilisateur vers la mainWindow grace a des modifications de app.xaml.cs

--- NewStarTicket/NewStarTicket/App.xaml.cs
+++ NewStarTicket/NewStarTicket/App.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System.Configuration;
 using System.Data;
 using System.Windows;
+using NewStarTicket.Views;
 
 namespace NewStarTicket
 {
@@ -9,6 +10,20 @@ namespace NewStarTicket
     /// </summary>
     public partial class App : Application
     {
+        protected void ApplicationStart(object sender, StartupEventArgs e)
+        {
+            var loginView = new LoginView();
+            loginView.Show();
+            loginView.IsVisibleChanged += (s, ev) =>
+            {
+                if (loginView.IsVisible == false && loginView.IsLoaded)
+                {
+                    var mainWindow = new MainWindow();
+                    mainWindow.Show();
+                    loginView.Close();
+                }
+            };
+        }
     }
 
 }
